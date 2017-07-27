45d96b5932b491a34a410b3433897027794c61df
Tyler <tbobik91@gmail.com>
2017-07-27 14:19:34 -0700

    Added Average Net Income tab and viz

--- src/App.js
+++ src/App.js
@@ -75,6 +75,12 @@ class App extends Component {
                 options={options}
               />
             </Tab>
+              <Tab eventKey={6} title="Average Net Price">
+              <TableauReport
+                url='https://public.tableau.com/views/AverageNetPrice/Sheet1?:embed=y&:display_count=yes'
+                options={options}
+              />
+            </Tab>
           </Tabs>
         )}/>
         <Route exact path='/calculate' render={({ history }) => (
